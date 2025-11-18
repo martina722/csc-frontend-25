80406014c62972855025cdd051b401950799da69
martina722 <marticm26@gmail.com>
2025-11-18 17:24:24 +0100

    modifiche

---
+++ 02 - Linguaggio PHP/text.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello, World!";
+
+    $variabile = 42;
+    echo "The answer is: $variabile";
+
+    if ($variabile > 40) {
+        echo " - Greater than 40";
+    } else {
+        echo " - 40 or less";
+    }
+    ?>
+</body>
+</html>
